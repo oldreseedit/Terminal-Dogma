59bca8bfc5b7898019f3ddc2e48e51b5f292f0fe
AlessandroFlati <alessandro.flati@gmail.com>
2015-11-23 20:18:44 +0100

    Fix e prova per exp.js

--- angular-modules/exp.js
+++ angular-modules/exp.js
@@ -1,10 +1,24 @@
-main.controller('expController',['$http',function($http){
+main.controller('expController',['$http','inform',function($http,inform){
 	var self = this;
 	
 	self.add = function(userID, xp, courseID, description)
 	{
+		var data= {userID: userID, exp: xp};
+		if(courseID) data.courseID = courseID;
+		if(description) data.description = description;
 		
-		console.log(userID,xp,courseID,description);
+		$http.post('users/add_exp',data).then(function(response)
+				{
+//					if(data.error) 
+					console.log(response.data);
+					
+				},function(error)
+				{
+
+				});
+		
+		console.log(data);
 	};
+		
 	
 }]);
