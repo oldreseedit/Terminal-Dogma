88670ec0bbccd83dbfaddf6c2f99bfd40fc156c5
AlessandroFlati <alessandro.flati@gmail.com>
2015-11-24 19:33:42 +0100

    Minor fixes

--- application/views/courses/course.php
+++ application/views/courses/course.php
@@ -1,7 +1,7 @@
-<div id="singleCourse">
+<div id="singleCourse" ng-init="course.init()">
     <div class="container">
         <div gridster>
-            <div class="gridster-item bg-light-grey" gridster-item="course.customItemMap" ng-repeat="item in course.items" id="gridsterItem{{index}}" ng-init="index = $index">
+            <div class="gridster-item bg-light-grey" gridster-item="course.customItemMap" id="gridsterItem{{index}}" ng-repeat="item in course.items" ng-init="index = $index">
                 <div class="container">
                     <div class="course-panel-title" ng-class="item.bgColour">
                         <h4 ng-bind="item.title"></h4>
@@ -10,7 +10,7 @@
                         <div class="container" id="gridsterItem{{index}}View">
                             <!--<perfect-scrollbar class="scrollbar-wrapper" suppressScrollX="true">-->
                             <!--    <div class="scrollbar">-->
-                                    <div ng-include="item.templateUrl" ng-if="item.ready()" onload="course.resize(index)"></div>
+                                    <div ng-include="item.templateUrl" ng-if="course.isReady(item.id)"></div>
                             <!--    </div>-->
                             <!--</perfect-scrollbar>-->
                         </div>
