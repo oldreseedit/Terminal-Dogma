cb197a0aec75ca88d65ba5ac3a1a61c1f7d6ef2b
AlessandroFlati <alessandro.flati@gmail.com>
2015-11-28 09:14:49 +0100

    Minor fixes

--- templates/profile-summary.php
+++ templates/profile-summary.php
@@ -2,15 +2,15 @@
 	<img src="imgs/leaf.png" class="resp-img" />
 </div>
 <div class="profile-name-level-xp">
-	<span ng-bind="user.username" fittext="0.8"></span>
+	<span ng-bind="profile.username" fittext="0.8"></span>
 	<p>
-		Seed Level: <span ng-bind="user.level"></span>
+		Seed Level: <span ng-bind="profile.level"></span>
 	</p>
 	<div class="profile-xp">
 		<div class="container">
-			<uib-progressbar value="user.currentXP" max="user.requiredXP"
-				type="user.xpBarTypes()" title="Esperienza"> <span
-				ng-bind="user.currentXP"></span>/<span ng-bind="user.requiredXP"></span>
+			<uib-progressbar value="profile.currentXP" max="profile.requiredXP"
+				type="profile.xpBarTypes()" title="Esperienza"> <span
+				ng-bind="profile.currentXP"></span>/<span ng-bind="profile.requiredXP"></span>
 			</uib-progressbar>
 		</div>
 	</div>
