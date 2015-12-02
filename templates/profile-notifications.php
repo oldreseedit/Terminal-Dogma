a123e9ed282c21417dd445dea68b0feed9b2e443
AlessandroFlati <alessandro.flati@gmail.com>
2015-12-02 22:29:59 +0100

    #5

--- templates/profile-notifications.php
+++ templates/profile-notifications.php
@@ -1,5 +1,5 @@
 <ul class="list-group">
-	<li class="profile-notification list-group-item" ng-repeat="notification in profile.notifications" ng-class="{'list-group-item-success':!profile.isSeen(notification)}">
+	<li class="profile-notification list-group-item" ng-repeat="notification in profile.notifications" ng-class="{'list-group-item-success':!profile.isSeen(notification)}" ng-mouseover="profile.seeNotification(notification)">
 		<span class="bold" ng-bind="profile.getTitleOfNotification(notification)"></span><br />
 		<span ng-bind="notification.text"></span>
 	</li>
