f44fa626efc0eca898830a0209ee080929fc85eb
tizianoflati <tiziano.flati@gmail.com>
2015-12-16 22:39:01 +0100

    Added the privacy link in the cookie consent banner

--- application/views/basics/head.php
+++ application/views/basics/head.php
@@ -138,7 +138,7 @@
     
     <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
     <script type="text/javascript">
-        window.cookieconsent_options = {"message":"Questo sito utilizza i cookie per garantire la miglior esperienza di navigazione possibile.","dismiss":"Ok, ok. Ora togliti.","learnMore":"","link":"","theme":"dark-floating"};
+        window.cookieconsent_options = {"message":"Questo sito utilizza i cookie per garantire la miglior esperienza di navigazione possibile.","dismiss":"Ok, ok. Ora togliti.","learnMore":"Maggiori informazioni","link":"#!/privacy","theme":"dark-floating"};
     </script>
     
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
