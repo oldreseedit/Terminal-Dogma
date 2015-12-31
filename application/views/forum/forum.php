2f7b85fd64d504b2b4146240226df8711fcf585b
tizianoflati <tiziano.flati@gmail.com>
2015-12-31 13:47:37 +0100

    Created the thread mockup

--- application/views/forum/forum.php
+++ application/views/forum/forum.php
@@ -1,5 +1,37 @@
 <div id="forum">
 	<h1>Forum di reSeed</h1>
+	
+	<div class="panel panel-default">
+		<div class="panel-body">
+			<button type="button" class="btn btn-success background-image-none bg-water">Crea una nuova discussione</button>
+		</div>
+	</div>
+	
+	<div class="container thread">
+		<div class="panel panel-default">
+			<div class="panel-heading"><h2 ng-bind="forum.discussionTitle"></h2></div>
+			<div class="panel-body">
+				<div>
+					<div class="container thread-message" ng-repeat="threadMessage in forum.threadMessages">
+						<div class="col-xs-3 message-author">
+							<img class="avatar" ng-src="{{threadMessage.author.avatar || 'https://www.huddbaits.com/img/300/0/assets/images/store/'}}">
+							<div><strong>Username: </strong><span ng-bind="threadMessage.author.username"></span></div>
+							<div>Gruppo: <span ng-bind="threadMessage.author.group"></span></div>
+							<div>Messaggi: <span ng-bind="threadMessage.author.messages"></span></div>
+							<div>Punteggio: <span ng-bind="threadMessage.author.score"></span></div>
+						</div>
+						<div class="col-xs-9">
+							<div class="thread-message-publication">Pubblicato il:
+								<span ng-bind="threadMessage.message.timestamp || 'N/A'"></span>
+							</div>
+							<div class="thread-text"><span ng-bind-html="threadMessage.message.text"></span></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		
 	<div class="container">
 		<div class="panel panel-default">
 			<div class="panel-heading"><h2>Discussioni aperte</h2></div>
@@ -9,10 +41,12 @@
 					    <tr>
 					    	<th></th>
 					    	<th class="forum-description"><p>Descrizione</p></th>
+					    	<th><p>Autore</p></th>
 						    <th><p>Thread</p></th>
 						    <th><p>Visite</p></th>
 						    <th><p>Risposte</p></th>
 						    <th><p>Partecipanti</p></th>
+						    <th><p>Ultimo messaggio</p></th>
 					    </tr>
 					    <tr ng-repeat="discussion in forum.discussions">
 					        <td><a href="forum/discussion/{{discussion.id}}"><i class="fa {{discussion.icon}}"></i></a></td>
@@ -22,10 +56,19 @@
 						        	<p ng-bind="discussion.description"></p>
 						        </div>
 					        </td>
+					        <td ng-bind="discussion.author"></td>
 					        <td ng-bind="discussion.threads"></td>
 					        <td ng-bind="discussion.views"></td>
 					        <td ng-bind="discussion.replies"></td>
 					        <td ng-bind="discussion.participants"></td>
+					        <td>
+					        	<div ng-if="discussion.lastMessage.timestamp">
+						        	<div ng-bind="discussion.lastMessage.timestamp"></div>
+							        <span>da parte di</span>
+							        <div ng-bind="discussion.lastMessage.author"></div>
+						        </div>
+						        <div ng-if="!discussion.lastMessage.timestamp"><p>N/A</p></div>
+					        </td>
 					    </tr>
 					</table>
 				</div>
--- js/controllers/forum.js
+++ js/controllers/forum.js
@@ -7,36 +7,97 @@ main.controller('forumController',['utilities','$scope','$http','$server','$rout
     /* CONFIG */
     self.username = $cookies.get('username');
     
+    self.discussionTitle = "Titolo discussione";
+    self.threadMessages = {
+    		0: {
+    			author: {
+    				username: "Titto",
+    				avatar: "http://www.animeemanga.it/wp-content/uploads/pokemon/charmeleon.jpg",
+    				group: "admin",
+    				messages: "102",
+    				level: "11",
+    				score: "302848283923",
+    			},
+    			message: {
+    				text: "Benvenuti a <b>tutti</b>",
+    				timestamp: "",
+    			}
+    		},
+    		1: {
+    			author: {
+    				username: "Student1",
+    				avatar: "",
+    				group: "user",
+    				messages: "12",
+    				level: "4",
+    				score: "382773",
+    			},
+    			message: {
+    				text: "Ciao!",
+    				timestamp: "",
+    			}
+    		},
+    		2: {
+    			author: {
+    				username: "Student1",
+    				avatar: "",
+    				group: "user",
+    				messages: "20",
+    				level: "7",
+    				score: "238357",
+    			},
+    			message: {
+    				text: "Salve, prof!",
+    				timestamp: "",
+    			}
+    		}    		
+    }
+    
     self.discussions = {
     		0: {
     			id: 'disc1',
     			icon: 'fa-comment',
     			title: 'Discussione 1',
     			description: 'Description 1',
+    			author: 'Minnie',
     			threads: '30',
     			views: '10',
     			replies: '5',
     			participants: '2',
+    			lastMessage: {
+    				timestamp: "ieri",
+    				author: "Chicco"
+    			}
     			},
 			1: {
 				id: 'disc2',
     			icon: 'fa-comment',
     			title: 'Discussione 2',
     			description: 'Long description 2',
+    			author: 'Pippo',
     			threads: '10',
     			views: '300',
     			replies: '210',
     			participants: '28',
+    			lastMessage: {
+    				timestamp: "l'altro ieri",
+    				author: "Titto"
+    			}
     			},
 			2: {
 				id: 'disc3',
     			icon: 'fa-comment',
     			title: 'Discussione 3',
     			description: 'Very very very very very very very very very very very very very very very very very long description 3',
+    			author: 'Topolino',
     			threads: '1',
     			views: '1',
-    			replies: '1',
+    			replies: '0',
     			participants: '1',
+    			lastMessage: {
+    				timestamp: "",
+    				author: ""
+    			}
     			}
     };
 }]);
