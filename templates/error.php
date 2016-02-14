f2d22bfd4dafe0440457788516e7ba576cd6b66d
AlessandroFlati <alessandro.flati@gmail.com>
2016-02-14 16:50:09 +0100

    Minor fix

--- templates/error.php
+++ templates/error.php
@@ -1,4 +1,4 @@
 <div class="in-main">
-	<h1><i class="text-danger fa fa-exclamation-triangle"></i> <?php echo $heading; ?></h1>
-	<p><?php echo $message; ?></p>
+	<h3><i class="text-danger fa fa-exclamation-triangle"></i></h3><h3>Errore 404: Pagina inesistente</h3>
+	<p>Il segreto che cercavi non si trova qui...</p>
 </div>
