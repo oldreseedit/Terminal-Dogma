ec42d8284d123fdf6b40f239ebe6b9843ef7fa93
AlessandroFlati <alessandro.flati@gmail.com>
2016-03-06 20:28:43 +0100

    minor fix

--- templates/course-description.php
+++ templates/course-description.php
@@ -6,15 +6,6 @@
             		<uib-slide>
             			<img src="imgs/carousel/provaCarousel.png"/>
             		</uib-slide>
-            		<uib-slide>
-            			<img src="imgs/header.png"/>
-            		</uib-slide>
-            		<uib-slide>
-            			<img src="imgs/footer.jpg" />
-            		</uib-slide>
-            		<uib-slide>
-            			<img src="imgs/mini-header.png" />
-            		</uib-slide>
             	</uib-carousel>
                 <p><span ng-bind-html="course.courseInfo.description"></span></p>
             </div>
