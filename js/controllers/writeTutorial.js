e7ce6d5d22e784c8fd944f57751a1f58d1380880
AlessandroFlati <alessandro.flati@gmail.com>
2016-03-13 18:37:57 +0100

    Revert "Major advance in Tutorial pages"
    This reverts commit aaa3068a227f14904261b8b6e71c2b43423f9454.

--- js/controllers/writeTutorial.js
+++ js/controllers/writeTutorial.js
@@ -1,8 +1,7 @@
-main.controller('writeTutorial',['$server','$scope','$uibModal','$route','inform','FileUploader',function($server,$scope,$uibModal,$route,inform,FileUploader){
+main.controller('writeTutorial',['$server','$scope','$uibModal','$route','inform',function($server,$scope,$uibModal,$route,inform){
 	var self = this;
 	
 	self.tutorialTitle = "";
-	self.tutorialID = "";
 	self.tutorialCourse = "";
 	self.tutorialShortDescription = "";
 	self.tutorialRequirements = "";
@@ -10,41 +9,27 @@ main.controller('writeTutorial',['$server','$scope','$uibModal','$route','inform
 	self.tutorialTags = "";
 	self.tutorialSeealso = "";
 	self.tutorialID = "";
-	self.tutorialImages = [];
-	
-	self.mode = "write";
 	
 	self.send = function()
 	{
-		var pending = self.uploader.getNotUploadedItems();
-		for(var i=0; i<pending.length; i++)
-		{
-			self.uploader(pending[i]);
-		}
-		
-		if(self.mode == "write")
+		if(self.tutorialID == "")
 		{
 			$server.post('tutorials/add',
 					{
-						tutorialID: self.tutorialID,
 						title: self.tutorialTitle,
-						id: self.tutorialID,
 						course: self.tutorialCourse.name,
 						description: self.tutorialShortDescription,
 						requirements: self.tutorialRequirements,
 						body: self.tutorialBody,
 						tags: self.tutorialTags,
-						seealso: self.tutorialSeealso,
-						images: self.tutorialImages
+						seealso: self.tutorialSeealso
 					}, true).then(
 		    		function(response)
 		    		{
 //		    			console.log(response);
 		    			
 		    			inform.add(response.data.description);
-		    			// self.tutorialID = response.data.tutorialID;
-		    			
-		    			self.mode = "modify";
+		    			self.tutorialID = response.data.tutorialID;
 		    		}
 		        );
 		}
@@ -77,29 +62,4 @@ main.controller('writeTutorial',['$server','$scope','$uibModal','$route','inform
     			self.courses = response.data;
     		}
         );
-	
-	self.uploader = new FileUploader({url: 'index.php/tutorials/upload_image'});
-	
-	self.uploader.onAfterAddingFile = function(fileItem) {
-		fileItem.formData.push({
-			title: self.tutorialTitle
-		});
-    };
-    
-    self.uploader.onCompleteItem = function(fileItem, response, status, headers) {
-        console.info('onCompleteItem', fileItem, response, status, headers);
-        
-        var message = response.description;
-        if(response.error) message += " (file: "+fileItem.file.name+")";
-        
-    	inform.add(message, {type: response.error ? 'danger' : 'success'});
-    	
-    	if(!response.error)
-    		self.tutorialImages.push(response.url);
-    };
-    
-    $scope.$watch(function(){return self.tutorialTitle;}, function(newValue, oldValue) {
-    	if(self.mode == "write")
-    		self.tutorialID = self.tutorialTitle.toLowerCase().split(" ").join("-");
-    });
 }]);
--- templates/write-tutorial.php
+++ templates/write-tutorial.php
@@ -1,83 +1,16 @@
 <div id="write-tutorial">
 	<div class="container">
 		<h1>Titolo del tutorial</h1>
-		<p>Fornisci un titolo. Max 100 caratteri.</p>
+		<p>Fornisci un titolo. Sii conciso.</p>
 		<input placeholder="Titolo" type="text" ng-model="writeTutorial.tutorialTitle"/>
-		<p><strong>Anteprima url:</strong> www.reseed.it/tutorial/<span ng-bind="writeTutorial.tutorialID"></span></p>		
 		
-		<h1>Corso del tutorial</h1>
+		<h1>Titolo del tutorial</h1>
 		<p>Scegli il corso per cui stai scrivendo questo tutorial (o in ogni caso quello che ritieni essere più vicino).</p>
 		<select name="subject" style="font-size: 1.3em; padding: 20px; font-weight: bold;"
 			ng-options="course.name for course in writeTutorial.courses"
 			ng-model="writeTutorial.tutorialCourse">
 			<option value="" disabled>Scegli il corso</option>
 		</select>
-		
-		<h1>Scegli le immagini:</h1>
-		<input type="file" nv-file-select uploader="writeTutorial.uploader"/><br/>
-<!--         <ul> -->
-<!--             <li ng-repeat="item in writeTutorial.uploader.queue"> -->
-<!--                 Name: <span ng-bind="item.file.name"></span><br/> -->
-<!--                 <button ng-click="item.upload()">upload</button> -->
-<!--             </li> -->
-<!--         </ul> -->
-        
-        			<table class="table">
-                        <thead>
-                            <tr>
-                                <th width="50%">Name</th>
-                                <th ng-show="writeTutorial.uploader.isHTML5">Size</th>
-                                <th ng-show="writeTutorial.uploader.isHTML5">Progress</th>
-                                <th>Status</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr ng-repeat="item in writeTutorial.uploader.queue">
-                                <td><strong>{{ item.file.name }}</strong></td>
-                                <td ng-show="writeTutorial.uploader.isHTML5" nowrap>{{ (item.file.size > 1024*1024 ? item.file.size/1024/1024 : item.file.size/1024) | number:2 }} {{item.file.size > 1024*1024 ? 'MB' : 'KB'}}</td>
-                                <td ng-show="writeTutorial.uploader.isHTML5">
-                                    <div class="progress" style="margin-bottom: 0;">
-                                        <div class="progress-bar" role="progressbar" ng-style="{ 'width': item.progress + '%' }"></div>
-                                    </div>
-                                </td>
-                                <td class="text-center">
-                                    <span ng-show="item.isSuccess"><i class="fa fa-icon fa-check"></i></span>
-                                    <span ng-show="item.isCancel"><i class="fa fa-icon fa-ban"></i></span>
-                                    <span ng-show="item.isError"><i class="fa fa-icon fa-times"></i></span>
-                                </td>
-                                <td nowrap>
-                                    <button type="button" class="btn btn-success btn-xs" ng-click="item.upload()" ng-disabled="item.isReady || item.isUploading || item.isSuccess">
-                                        <span class="fa fa-icon fa-upload"></span> Upload
-                                    </button>
-                                    <button type="button" class="btn btn-warning btn-xs" ng-click="item.cancel()" ng-disabled="!item.isUploading">
-                                        <span class="fa fa-icon fa-ban"></span> Cancel
-                                    </button>
-                                    <button type="button" class="btn btn-danger btn-xs" ng-click="item.remove()">
-                                        <span class="fa fa-icon fa-trash"></span> Remove
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-
-                    <div>
-                        <div>
-                            Queue progress:
-                            <div class="progress" style="">
-                                <div class="progress-bar" role="progressbar" ng-style="{ 'width': writeTutorial.uploader.progress + '%' }"></div>
-                            </div>
-                        </div>
-                        <button type="button" class="btn btn-success btn-s" ng-click="writeTutorial.uploader.uploadAll()" ng-disabled="!writeTutorial.uploader.getNotUploadedItems().length">
-                            <span class="fa fa-icon fa-upload"></span> Upload all
-                        </button>
-                        <button type="button" class="btn btn-warning btn-s" ng-click="writeTutorial.uploader.cancelAll()" ng-disabled="!writeTutorial.uploader.isUploading">
-                            <span class="fa fa-icon fa-ban"></span> Cancel all
-                        </button>
-                        <button type="button" class="btn btn-danger btn-s" ng-click="writeTutorial.uploader.clearQueue()" ng-disabled="!writeTutorial.uploader.queue.length">
-                            <span class="fa fa-icon fa-trash"></span> Remove all
-                        </button>
-                    </div>
 		
 		<h1>Breve descrizione</h1>
 		<p>Fornisci una breve descrizione, di 2-3 righe del tutorial.</p>
@@ -100,7 +33,7 @@
 		<div placeholder="See also" text-angular ng-model="writeTutorial.tutorialSeealso"></div>
 		
 		<div class="buttons text-center">
-			<button class="btn btn-success" ng-disabled="writeTutorial.tutorialTitle == '' || writeTutorial.tutorialBody == ''" ng-click="writeTutorial.send()">{{writeTutorial.mode == 'write' ? 'Salva tutorial' : 'Aggiorna tutorial'}}</button>
+			<button class="btn btn-success" ng-disabled="writeTutorial.tutorialTitle == '' || writeTutorial.tutorialBody == ''" ng-click="writeTutorial.send()">{{writeTutorial.tutorialID == '' ? 'Salva tutorial' : 'Aggiorna tutorial'}}</button>
 		</div>
 	</div>
 </div>
