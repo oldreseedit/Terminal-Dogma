d9ebdaab87e24b10108c377575112e559a75fc88
AlessandroFlati <alessandro.flati@gmail.com>
2016-04-15 09:33:50 +0200

    minor fix

--- templates/eventAd.php
+++ templates/eventAd.php
@@ -3,7 +3,7 @@
 		<h3 class="jungle text-center">Se ancora non sei convinto, vieni a trovarci!</h3><br />
 		<a href="https://www.facebook.com/events/1746771425592086/" target="_blank"><img class="float" style="max-width:50%; margin-right: 1em; margin-bottom: 1em;" src="imgs/jjAd.jpg"></a>
 		<a href="https://www.facebook.com/events/1746771425592086/" target="_blank"><h5><span class="fa fa-calendar fa-2x middle"></span><span class="middler"></span><span class="offset-1 middle">Dietro le quinte di un videogioco di successo</span></h5></a><br/>
-		<p><b>Quando:</b> 11.04.2016 - Dalle 15:00 alle 18:00 </p>
+		<p><b>Quando:</b> 15.04.2016 - Dalle 15:00 alle 18:00 </p>
 		<p><b>Dove:</b> <a href="https://goo.gl/maps/Y2k68kuJuaz" target="_blank">Via Tiburtina 912</a> - zona Rebibbia</p><br />
 		<p>Conoscete Jetpack Joyride? Il gioco mobile da milioni di download? L'infinite runner dal successo planetario? Quello.</p>
 		<p>Vi siete mai chiesti cosa lo ha reso un gran successo? O che problemi hanno dovuto affrontare gli sviluppatori nel realizzarlo? O ancora, pensate sia difficile programmare un gioco del genere? (Spoiler: no!)</p>
